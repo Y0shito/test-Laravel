20d9033ab42c272c7ac12c6daac6677ff7b0b018
Yoshito <yoshityan.ibatyan@gmail.com>
2021-05-31 17:57:37 +0900

    Bookmarkの不要なメソッド削除

--- app/Http/Controllers/BookmarkController.php
+++ app/Http/Controllers/BookmarkController.php
@@ -10,14 +10,5 @@ use Illuminate\Support\Facades\Auth;
 
 class BookmarkController extends Controller
 {
-    // public function myBookmarks(){
-    //     $bookmarks = Bookmark::where('user_id', Auth::user()->id)->get();
-    //     return view('layouts.mypage', ['bookmarks' => $bookmarks]);
-    // }
-    
-    // public function db(Request $request){
-    //     $bookmarks = DB::select('select * from bookmarks');
-    //     return view('layouts.mypage',['bookmarks' => $bookmarks]);
-    // }
 
 }
