90251cc9c78b1db17b391fd593324ce03e66a253
Yoshito <yoshityan.ibatyan@gmail.com>
2021-06-03 23:36:56 +0900

    ブックマークボタン実装

--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -2,6 +2,8 @@
   .sort{display:flex; justify-content:space-between; align-items:center;}
   .sort ul {display:flex; list-style:none;}
   .sort li {padding:0 0 0 20px;}
+  .line {display:flex; justify-content:space-between; align-items:center;}
+  .line .Button {display:flex;}
 </style>
 
 @extends('layouts.template')
@@ -19,33 +21,33 @@
       <li><a href="/index?sort=created_at">投稿日順</a></li>
     </ul>
   </div>
-  <br>
-  @foreach($items as $item)
-    <h3><a href="article/id={{$item->id}}">{{$item->title}}</a></h3>
-    <p>{{$item->body}}</p>
     <br>
-    <p>閲覧数：{{$item->view}}&nbsp;
-      @if(isset($item->updated_at))
-        更新日：{{$item->updated_at->format('Y年m月d日') }}&nbsp;
-      @else
-        作成日：{{$item->created_at->format('Y年m月d日') }}&nbsp;
-      @endif
-    作成者：{{$item->author->name}}&nbsp;
-    ブックマーク数：{{$item->bookmarks->count()}}
-    </p>
-    <!-- <form action="edit" method="POST">
-      @csrf
-      <input type="hidden" name="id" value="{{$item->id}}">
-      <input type="submit" value="編集">
-    </form>
-    <form action="delete" method="POST">
-      @csrf
-      <input type="hidden" name="id" value="{{$item->id}}">
-      <input type="submit" value="削除">
-    </form>  -->
+    @foreach($items as $item)
+        <div class="line">
+            <h3><a href="article/id={{$item->id}}">{{$item->title}}</a></h3>
+            <div class="button">
+                @if (Auth::check())
+                    <form action="bmAdd" method="POST">
+                        @csrf
+                        <input type="hidden" name="id" value="{{$item->id}}">
+                        <input type="submit" value="ブックマーク({{$item->bookmarks->count()}})">
+                    </form>
+                @endif
+            </div>
+        </div>
+
+        <p>{{$item->body}}</p>
+        <br>
+        <p>閲覧数：{{$item->view}}&nbsp;
+        @if(isset($item->updated_at))
+            更新日：{{$item->updated_at->format('Y年m月d日') }}&nbsp;
+        @else
+            作成日：{{$item->created_at->format('Y年m月d日') }}&nbsp;
+        @endif
+        作成者：{{$item->author->name}}
+        </p>
     <br>
     <hr color="blue">
     <br>
   @endforeach
-<!-- ① -->
 @endsection
