e421bfcb96bafea5b775cf71291aa28a9064af53
Yoshito <yoshityan.ibatyan@gmail.com>
2021-06-03 22:55:41 +0900

    articleで投稿者、ブックマーク数実装

--- resources/views/layouts/article.blade.php
+++ resources/views/layouts/article.blade.php
@@ -7,11 +7,15 @@
 @section('content')
 <h3>{{$article->title}}</h3>
 <br>
-<p>閲覧数：{{$article->view}}&nbsp;
+<p>
+  閲覧数：{{$article->view}}&nbsp;
   @if(isset($article->updated_at))
     更新日：{{$article->updated_at->format('Y年m月d日')}}&nbsp;
   @endif
-作成日：{{$article->created_at->format('Y年m月d日')}}</p>
+  作成日：{{$article->created_at->format('Y年m月d日')}}
+  作成者：{{$article->author->name}}&nbsp;
+  ブックマーク数：{{$article->bookmarks->count()}}
+</p>
 <br>
 <p>{{$article->body}}</p>
 <br>
