ce0acdd76fac3911e60cbd925e54bb0ca1d963a7
Yoshito <yoshityan.ibatyan@gmail.com>
2021-06-03 22:58:21 +0900

    BookmarkにbmAdd、bmRemove、routeに実装

--- app/Http/Controllers/BookmarkController.php
+++ app/Http/Controllers/BookmarkController.php
@@ -10,8 +10,21 @@ use Illuminate\Support\Facades\Auth;
 
 class BookmarkController extends Controller
 {
-//   public function myBookmarks(Request $request){
-//     $myBookmarks = Bookmark::where('user_id', Auth::user()->id)->first();
-//     return view('layouts.mypage', ['myBookmarks' => $myBookmarks]);
-// }
+  public function bmAdd(Request $request){
+    $bookmark = new Bookmark;
+    $bookmark->user_id = Auth::user()->id;
+    $bookmark->article_id = $request->id;
+    $bookmark->created_at = Carbon::now('Asia/Tokyo');
+    $bookmark->updated_at = '0000-00-00 00:00:00';
+    $bookmark->save();
+
+    return redirect('/index');
+  }
+
+  public function bmRemove(Request $request){
+    $bookmark = Bookmark::find($request->id);
+    $bookmark->delete();
+
+    return redirect('/index');
+}
 }
