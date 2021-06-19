44b2bd36006d72d19e777908a8f284a06fb497a0
Yoshito <yoshityan.ibatyan@gmail.com>
2021-06-20 00:50:04 +0900

    カテゴリ定数とドロップダウンメニュー実装

--- resources/views/layouts/preview.blade.php
+++ resources/views/layouts/preview.blade.php
@@ -9,32 +9,40 @@
     記事はまだ投稿されていません。記事内容をご確認ください
 </div>
 
-<div class="d-flex">
-    <div>
-        <h3>{{session()->get('title')}}</h3>
+<form action="" method="post">
+    @csrf
+    <div class="d-flex">
+        <div>
+            <h3>{{session()->get('title')}}</h3>
+        </div>
+        <div class="ml-auto">
+            <input class="btn btn-secondary btn-sm" type="submit" value="ブックマーク(--)" disabled>
+        </div>
     </div>
-    <div class="ml-auto">
-        <input class="btn btn-secondary btn-sm" type="submit" value="ブックマーク(--)" disabled>
-    </div>
-</div>
-<p>
-    閲覧数：--&nbsp;
-    作成日：{{Carbon\Carbon::now('Asia/Tokyo')->format('Y年m月d日')}}&nbsp;
-    作成者：{{Auth::user()->name}}
-</p>
-<br>
-<p>{!! nl2br(e(session()->get('body'))) !!}</p>
+    <p>
+        閲覧数：--&nbsp;
+        作成日：{{Carbon\Carbon::now('Asia/Tokyo')->format('Y年m月d日')}}&nbsp;
+        作成者：{{Auth::user()->name}}
+    </p>
+    <br>
+    <p>{!! nl2br(e(session()->get('body'))) !!}</p>
 
-<div class="d-flex justify-content-center mb-3">
-    <form action="" method="post">
+    <select name="category" class="custom-select mb-3">
+        @foreach (config('const.category') as $key => $value)
+            <option value="{{$key}}">{{$value}}</option>
+        @endforeach
+    </select>
+
+    <div class="d-flex justify-content-center mb-3">
         <div class="button">
-            @csrf
             <input class="btn btn-secondary btn-lg" type="submit" class="draft" value="下書きへ保存">
             <input type="hidden" name="proc" value="draft">
 
             <input class="btn btn-primary btn-lg" type="submit" class="add" value="投稿">
             <input type="hidden" name="proc" value="add">
         </div>
-    </form>
+    </div>
+
+</form>
 </div>
 @endsection
