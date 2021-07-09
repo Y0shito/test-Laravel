1f5e4db8cc43b3495dd70507e42bb284fbd9b90b
Yoshito <yoshityan.ibatyan@gmail.com>
2021-07-09 17:27:40 +0900

    マイページにbootstrapタブ実装

--- resources/views/layouts/mypage.blade.php
+++ resources/views/layouts/mypage.blade.php
@@ -24,104 +24,130 @@
 </div>
 <br>
 
-{{-- ここから執筆記事 --}}
-<h3>公開中の記事：{{count($items)}}件</h3>
 
-@foreach($items as $item)
-<div class="card my-3">
-    <div class="card-header d-inline-flex pb-0">
-        <h3><a href="article/id/{{$item->id}}">{{$item->title}}</a></h3>
-        <div class="ml-auto d-inline-flex">
-            <form action="edit" method="POST">
-                @csrf
-                <input type="hidden" name="id" value="{{$item->id}}">
-                <input class="btn btn-primary btn-sm" type="submit" value="編集">
-            </form>
 
-            @if($item->open == 1)
-            <form action="close" method="POST">
-                @csrf
-                <input type="hidden" name="id" value="{{$item->id}}">
-                <input class="btn btn-secondary btn-sm" type="submit" value="非公開にする">
-            </form>
-            @else
-            <form action="open" method="POST">
-                @csrf
-                <input type="hidden" name="id" value="{{$item->id}}">
-                <input class="btn btn-success btn-sm" type="submit" value="公開する">
-            </form>
-            @endif
+<ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
+    <li class="nav-item">
+        <a class="nav-link active" id="pills-home-tab" data-toggle="pill" href="#pills-home" role="tab"
+            aria-controls="pills-home" aria-selected="true">記事</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" id="pills-profile-tab" data-toggle="pill" href="#pills-profile" role="tab"
+            aria-controls="pills-profile" aria-selected="false">ブックマーク</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" id="pills-contact-tab" data-toggle="pill" href="#pills-contact" role="tab"
+            aria-controls="pills-contact" aria-selected="false">フォロー</a>
+    </li>
+</ul>
 
-            <form action="delete" method="POST">
-                @csrf
-                <input type="hidden" name="id" value="{{$item->id}}">
-                <input class="btn btn-danger btn-sm" type="submit" value="削除">
-            </form>
+<div class="tab-content" id="pills-tabContent">
+    <div class="tab-pane fade show active" id="pills-home" role="tabpanel">
+        {{-- ここから執筆記事 --}}
+        <h3>公開中の記事：{{count($items)}}件</h3>
+
+        @foreach($items as $item)
+        <div class="card my-3">
+            <div class="card-header d-inline-flex pb-0">
+                <h3><a href="article/id/{{$item->id}}">{{$item->title}}</a></h3>
+                <div class="ml-auto d-inline-flex">
+                    <form action="edit" method="POST">
+                        @csrf
+                        <input type="hidden" name="id" value="{{$item->id}}">
+                        <input class="btn btn-primary btn-sm" type="submit" value="編集">
+                    </form>
+
+                    @if($item->open == 1)
+                    <form action="close" method="POST">
+                        @csrf
+                        <input type="hidden" name="id" value="{{$item->id}}">
+                        <input class="btn btn-secondary btn-sm" type="submit" value="非公開にする">
+                    </form>
+                    @else
+                    <form action="open" method="POST">
+                        @csrf
+                        <input type="hidden" name="id" value="{{$item->id}}">
+                        <input class="btn btn-success btn-sm" type="submit" value="公開する">
+                    </form>
+                    @endif
+
+                    <form action="delete" method="POST">
+                        @csrf
+                        <input type="hidden" name="id" value="{{$item->id}}">
+                        <input class="btn btn-danger btn-sm" type="submit" value="削除">
+                    </form>
+                </div>
+            </div>
+
+            <div class="card-body">
+                <p class="card-text">{{$item->body}}</p>
+            </div>
+
+            <div class="card-footer">
+                <p class="card-text">
+                    閲覧数：{{$item->view}}&nbsp;
+                    @if(isset($item->updated_at))
+                    更新日：{{$item->updated_at->format('Y年m月d日') }}&nbsp;
+                    @else
+                    作成日：{{$item->created_at->format('Y年m月d日') }}&nbsp;
+                    @endif
+                    ブックマーク数：{{$item->bookmarks->count()}}&nbsp;
+                    カテゴリ：{{$item->category}}
+                </p>
+            </div>
+        </div>
+        @endforeach
+
+        <div class="d-flex justify-content-center">
+            {{$items->links()}}
+        </div>
+
+    </div>
+
+    <div class="tab-pane fade" id="pills-profile" role="tabpanel">
+        {{-- ここからブックマークした記事 --}}
+        <h3>ブックマークした記事：{{count($bookmarks)}}件</h3>
+
+        @foreach ($bookmarks as $bookmark)
+        <div class="card my-3">
+            <div class="card-header d-inline-flex pb-0">
+                <h3><a href="article/id={{$bookmark->articles->id}}">{{$bookmark->articles->title}}</a></h3>
+            </div>
+
+            <div class="card-body">
+                <p class="card-text">{{$bookmark->articles->body}}</p>
+            </div>
+
+            <div class="card-footer">
+                <p class="card-text">
+                    閲覧数：{{$bookmark->articles->view}}&nbsp;
+                    @if(isset($bookmark->articles->updated_at))
+                    更新日：{{$bookmark->articles->updated_at->format('Y年m月d日') }}&nbsp;
+                    @else
+                    作成日：{{$bookmark->articles->created_at->format('Y年m月d日') }}&nbsp;
+                    @endif
+                    作成者：{{$bookmark->articles->author->name}}&nbsp;
+                    カテゴリ：{{$bookmark->articles->category}}
+                </p>
+            </div>
+        </div>
+        @endforeach
+
+        <div class="d-flex justify-content-center">
+            {{$bookmarks->links()}}
         </div>
     </div>
 
-    <div class="card-body">
-        <p class="card-text">{{$item->body}}</p>
+    <div class="tab-pane fade" id="pills-contact" role="tabpanel">
+        {{-- ここからブックマークした記事 --}}
+        <h3>フォローしている人：{{count($getFollows)}}件</h3>
+        <ul class="list-gloup">
+            @foreach ($getFollows as $item)
+            <li class="list-group-item">{{$item->name}}</li>
+            @endforeach
+        </ul>
     </div>
 
-    <div class="card-footer">
-        <p class="card-text">
-            閲覧数：{{$item->view}}&nbsp;
-            @if(isset($item->updated_at))
-            更新日：{{$item->updated_at->format('Y年m月d日') }}&nbsp;
-            @else
-            作成日：{{$item->created_at->format('Y年m月d日') }}&nbsp;
-            @endif
-            ブックマーク数：{{$item->bookmarks->count()}}&nbsp;
-            カテゴリ：{{$item->category}}
-        </p>
-    </div>
 </div>
-@endforeach
-
-<div class="d-flex justify-content-center">
-    {{$items->links()}}
-</div>
-
-{{-- ここからブックマークした記事 --}}
-<h3>ブックマークした記事：{{count($bookmarks)}}件</h3>
-
-@foreach ($bookmarks as $bookmark)
-<div class="card my-3">
-    <div class="card-header d-inline-flex pb-0">
-        <h3><a href="article/id={{$bookmark->articles->id}}">{{$bookmark->articles->title}}</a></h3>
-    </div>
-
-    <div class="card-body">
-        <p class="card-text">{{$bookmark->articles->body}}</p>
-    </div>
-
-    <div class="card-footer">
-        <p class="card-text">
-            閲覧数：{{$bookmark->articles->view}}&nbsp;
-            @if(isset($bookmark->articles->updated_at))
-            更新日：{{$bookmark->articles->updated_at->format('Y年m月d日') }}&nbsp;
-            @else
-            作成日：{{$bookmark->articles->created_at->format('Y年m月d日') }}&nbsp;
-            @endif
-            作成者：{{$bookmark->articles->author->name}}&nbsp;
-            カテゴリ：{{$bookmark->articles->category}}
-        </p>
-    </div>
-</div>
-@endforeach
-
-<div class="d-flex justify-content-center">
-    {{$bookmarks->links()}}
-</div>
-
-
-{{-- ここからブックマークした記事 --}}
-<h3>フォローしている人：{{count($getFollows)}}件</h3>
-<ul class="list-gloup">
-    @foreach ($getFollows as $item)
-    <li class="list-group-item">{{$item->name}}</li>
-    @endforeach
-</ul>
 
 @endsection
--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -5,6 +5,9 @@
     <meta charset="UTF-8">
     <title>@yield('title')</title>
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
 </head>
 
 <body style="padding-top:70px">
