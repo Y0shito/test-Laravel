8890434eb2ad99625a65e35b4a094abdd4c22a8d
Yoshito <yoshityan.ibatyan@gmail.com>
2021-08-23 14:48:07 +0900

    article.blade省コード化

--- resources/views/layouts/article.blade.php
+++ resources/views/layouts/article.blade.php
@@ -10,25 +10,19 @@
         <h3>{{$article->title}}</h3>
     </div>
     <div class="ml-auto">
-        @if (Auth::check() AND !($article->author_id == Auth::user()->id))
-        @if ($article->bookmarks()->where('user_id',Auth::user()->id)->exists())
-        {{-- 記事からBookmark引っ張り、その中に自分のidあれば「外す」ボタン --}}
-        <form action="bmRemove" method="POST">
-            @csrf
-            <input class="btn btn-primary btn-sm" type="submit" value="ブックマーク中({{$article->bookmarks->count()}})">
-            <input type="hidden" name="id" value="{{$article->id}}">
-        </form>
-        @else
-        <form action="bmAdd" method="POST">
-            @csrf
-            <input class="btn btn-outline-primary btn-sm" type="submit"
-                value="ブックマークする({{$article->bookmarks->count()}})">
-            <input type="hidden" name="id" value="{{$article->id}}">
-        </form>
-        @endif
-        @else
-        <input class="btn btn-secondary btn-sm" type="submit" value="ブックマーク({{$article->bookmarks->count()}})"
-            disabled>
+        @if (Auth::check() AND !($article->author_id == Auth::id()))
+            <form method="POST">
+                @csrf
+                @if ($article->bookmarks()->where('user_id',Auth::id())->exists())
+                    <button class="btn btn-primary btn-sm" type="submit" formaction="/bmRemove" name="id"
+                        value="{{$article->id}}">ブックマーク中</button>
+                @else
+                    <button class="btn btn-outline-primary btn-sm" type="submit" formaction="/bmAdd" name="id"
+                    value="{{$article->id}}">ブックマーク</button>
+                @endif
+            </form>
+            @else
+                <button class="btn btn-secondary btn-sm" disabled>ブックマーク</button>
         @endif
     </div>
 </div>
@@ -39,7 +33,8 @@
     @endif
     作成日：{{$article->created_at->format('Y年m月d日')}}&nbsp;
     カテゴリ：{{$article->category}}&nbsp;
-    作成者：{{$article->author->name}}
+    ブックマーク数：{{$article->bookmarks->count()}}&nbsp;
+    作成者：<a href="/user/{{$article->author->id}}">{{$article->author->name}}</a>
 </p>
 <br>
 <p>{!! nl2br(e($article->body)) !!}</p>
